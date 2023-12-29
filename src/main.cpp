8e0066391db8d099911b1be379c6292f7b9c9e0a
uforgot <uforgot@me.com>
2023-12-29 17:17:47 +0900

    glfw init 추가

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <spdlog/spdlog.h>
+#include <GLFW/glfw3.h>
 
 int main(int argc, const char** argv) {
-    std::cout << "hello, opengl" << std::endl;
+    SPDLOG_INFO("Start program");
+    
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+        
+    // if (!glfwInit()) {
+    //     const char* description = nullptr;
+    //     glfwGetError(&description);
+    //     SPDLOG_ERROR("failed to initialize glfw: {}", description);
+    //     return -1;
+    // }
+
+    // SPDLOG_INFO("Create glfw window");
+    // auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr);
+    // if (!window) {
+    //     SPDLOG_ERROR("failed to create glfw window");
+    //     glfwTerminate();
+    //     return -1;
+    // }
+
     return 0;
 }
