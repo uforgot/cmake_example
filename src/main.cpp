21bd0efc40a1f7fb796a95f9739d50ea95e4fa8f
uforgot <uforgot@me.com>
2024-01-02 10:42:58 +0900

    [add] add foundation library to cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+/* Ask for an OpenGL Core Context */
+#define GLFW_INCLUDE_GLCOREARB
+
 #include <iostream>
 #include <spdlog/spdlog.h>
 #include <GLFW/glfw3.h>
@@ -5,26 +8,34 @@
 int main(int argc, const char** argv) {
     SPDLOG_INFO("Start program");
     
-    GLFWwindow* window;
+    // #ifdef __APPLE__
+    // /* We need to explicitly ask for a 3.2 context on OS X */
+    // glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 3);
+    // glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 2);
+    // glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    // glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // #endif 
 
-    /* Initialize the library */
-    if (!glfwInit())
+    if (!glfwInit()) {
+        const char* description = nullptr;
+        glfwGetError(&description);
+        SPDLOG_ERROR("failed to initialize glfw: {}", description);
         return -1;
-        
-    // if (!glfwInit()) {
-    //     const char* description = nullptr;
-    //     glfwGetError(&description);
-    //     SPDLOG_ERROR("failed to initialize glfw: {}", description);
-    //     return -1;
-    // }
+    }
 
-    // SPDLOG_INFO("Create glfw window");
-    // auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr);
-    // if (!window) {
-    //     SPDLOG_ERROR("failed to create glfw window");
-    //     glfwTerminate();
-    //     return -1;
-    // }
+    SPDLOG_INFO("Create glfw window");
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr);
+    if (!window) {
+        SPDLOG_ERROR("failed to create glfw window");
+        glfwTerminate();
+        return -1;
+    }
 
+    SPDLOG_INFO("Start main loop");
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
